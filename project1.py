453deb2e7ed56ea65bb02ce71b27fa7bced3451e
Navathej.AR <navathej24@gmail.com>
2024-02-22 21:17:43 +0530

    first project

---
+++ project1.py
@@ -0,0 +1,29 @@
+ef add(x,y):
+    return x+y
+def subtract(x,y):
+    return X-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+            if y == 0:
+                return "Error!"
+            else:
+                return x / y
+print("Select operation:")
+print("1. Add")
+print("2. Substract")
+print("3. Multiply")
+print("4. Divide")
+choice = input("Enter choice (1/2/3/4): ")
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+if choice == '1':
+            print("result:", add(num1,num2))
+elif choice == '2':
+            print("result:", subtract(num1,num2))
+elif choice == '3':
+            print("Result:", multiply(num1,num2))
+elif choice == '4':
+            print("result:", divide(num1,num2))
+else:
+            print("Invalid input")
